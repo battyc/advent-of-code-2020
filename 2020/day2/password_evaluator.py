144638a3f41c6e6bd182bb69c023292bd9806ca3
Chris Batty <chris@batty.co>
2021-09-12 06:25:41 +0000

    Reorder functions for consistency

--- 2020/day2/password_evaluator.py
+++ 2020/day2/password_evaluator.py
@@ -3,6 +3,22 @@
 import re
 
 
+def main():
+    f = open("input", "r")
+    password_list = f.read().splitlines()
+    sled_total = 0
+    toboggan_total = 0
+    for p in password_list:
+        sled_total += evaluate_sled_policy_password(p)
+        toboggan_total += evaluate_toboggan_policy_password(p)
+    print("Sled Total: {}".format(sled_total))
+    print("Toboggan Total: {}".format(toboggan_total))
+
+
+if __name__ == "__main__":
+    main()
+
+
 def evaluate_sled_policy_password(password_line):
     rule_expression = r'([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)'
     low, high, letter, password = re.match(
@@ -22,19 +38,3 @@ def evaluate_toboggan_policy_password(password_line):
     if (p1 == letter and p2 != letter) or (p2 == letter and p1 != letter):
         return 1
     return 0
-
-
-def main():
-    f = open("input", "r")
-    password_list = f.read().splitlines()
-    sled_total = 0
-    toboggan_total = 0
-    for p in password_list:
-        sled_total += evaluate_sled_policy_password(p)
-        toboggan_total += evaluate_toboggan_policy_password(p)
-    print("Sled Total: {}".format(sled_total))
-    print("Toboggan Total: {}".format(toboggan_total))
-
-
-if __name__ == "__main__":
-    main()
