cd5bcb957f06aee1b8c66e7eeba5613794a0d516
Chris Batty <chris@batty.co>
2021-09-12 09:10:38 +0000

    Complete day 3

--- 2020/day3/tree_finder.py
+++ 2020/day3/tree_finder.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python
 
 def main():
-    """
-    Rules:
-    # is a tree
-    . is an open space
-
-    Task: count the trees you encounter by starting at 0,0
-    and moving 3 right 1 down.  Loop to beginning of row at the edges
-    """
     f = open("input", "r")
     tree_list = f.read()
-    tree_count = 0
-
-    # coordinates
-    x = 0
-    y = 0
-
     if tree_list:
         tree_list = tree_list.splitlines()
-        while y < len(tree_list):
-            tree_count += 1 if tree_list[y][x] == '#' else 0
-            y += 1
-            x += 3
-            x = x % 31
-    print(tree_count)
+        a = get_trees_in_path(1, 1, tree_list)
+        b = get_trees_in_path(3, 1, tree_list)
+        c = get_trees_in_path(5, 1, tree_list)
+        d = get_trees_in_path(7, 1, tree_list)
+        e = get_trees_in_path(1, 2, tree_list)
+    print("{}".format(a * b * c * d * e))
+
+
+def get_trees_in_path(x_incr, y_incr, tree_list):
+    tree_count = 0
+    x = 0
+    y = 0
+    while y < len(tree_list):
+        tree_count += 1 if tree_list[y][x] == '#' else 0
+        x += x_incr
+        y += y_incr
+        # mod so that we loop around when we reach the end of a row
+        x = x % 31
+    return tree_count
 
 
 if __name__ == "__main__":
