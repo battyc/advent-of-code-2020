b043bba86deec05dd8be1679ab87d0a4fa84fb50
Chris Batty <chris@batty.co>
2021-09-19 00:25:02 +0000

    Complete part 2

--- 2020/day5/binary_boarding.py
+++ 2020/day5/binary_boarding.py
@@ -32,10 +32,25 @@ def process_input(boarding_input, input_type):
     return output
 
 
+def find_my_seat(assigned_seats, id_list):
+    for row, col_nums in assigned_seats.items():
+        row_map = dict.fromkeys(list(range(0, 8)), 1)
+        if len(col_nums) < 8:
+            for seat in col_nums:
+                row_map.pop(seat)
+            if row_map:
+                for col_num in row_map.keys():
+                    id = (row * 8) + col_num
+                    if (id + 1) in id_list and (id - 1) in id_list:
+                        return id
+
+
 def main():
     f = open("input", "r", encoding="utf-8")
     boarding_assignments = f.read()
     if boarding_assignments:
+        assigned_seats = {}
+        id_list = []
         boarding_assignments = boarding_assignments.splitlines()
         highest_id = 0
         for ba in boarding_assignments:
@@ -44,9 +59,17 @@ def main():
             bp_id = (row * 8) + col
             if bp_id > highest_id:
                 highest_id = bp_id
+            if assigned_seats.get(row):
+                assigned_seats[row].append(col)
+                id_list.append(bp_id)
+            else:
+                assigned_seats[row] = [col]
+                id_list.append(bp_id)
+        my_seat = find_my_seat(assigned_seats, id_list)
         p = "Highest Seat ID: {}".format(
                 highest_id)
         print(p)
+        print("My Seat: {}".format(my_seat))
     f.close()
 
 
